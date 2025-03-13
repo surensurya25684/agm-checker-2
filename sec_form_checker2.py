a960460a639705d1199c6eef16378e4f2d31ce72
surensurya25684 <suren.surya@msci.com>
2025-03-13 09:57:54 +0530

    Update sec_form_checker2.py

--- sec_form_checker2.py
+++ sec_form_checker2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
-from xlsxwriter import Workbook
+from xlsxwriter import workbook
 from datetime import datetime
 from collections import defaultdict
 import io
